c6998fdec8de4282960e9efe8ab2d64cafd9acde
AsmaahHAqq <asmaah.a.haqq@gmail.com>
2022-12-06 01:32:55 -0500

    added registration and login fuctionalities

---
+++ app/Views/pages/login.php
@@ -0,0 +1,11 @@
+<form action="login" method="post">
+
+<p>Enter email</p>
+<p><input type="text" name="email"></p>
+
+<p>Enter password</p>
+<p><input type="text" name="password"></p>
+
+<p><input type="submit" name="login"></p>
+
+</form>
---
+++ app/Views/pages/registration.php
@@ -0,0 +1,16 @@
+<form action="registration" method="post">
+<p>Enter First Name</p>
+<p><input type="text" name="first_name"></p>
+
+<p>Enter last Name</p>
+<p><input type="text" name="last_name"></p>
+<p>Enter email</p>
+<p><input type="text" name="email"></p>
+
+<p>Enter password</p>
+<p><input type="text" name="password"></p>
+
+<p><input type="submit" name="registration"></p>
+
+</form>
+
